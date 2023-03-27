e8522312c2b0d7f501ee9d5450fc074a9a2e1011
In-Wess-Tation <skolekonto318@gmail.com>
2023-03-27 19:26:07 +0200

    progress 1

--- routes/animals.routes.js
+++ routes/animals.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Animal = require("../models/animals.model")
 
-
+//get all animals
 router.get("/animals", async function(request, response, next) {
     try {
         let result = await Animal.find();
@@ -11,5 +11,16 @@ router.get("/animals", async function(request, response, next) {
         return next(error);
     }
 })
+
+//get single animal by id
+router.get("/animals/:id", async function(request, response, next) {
+    try {
+        let result = await Animal.findById(request.params.id);
+        return response.status(200).json(result)
+    } catch (error) {
+        return next(error)
+    }
+})
+
 
 module.exports = router;
